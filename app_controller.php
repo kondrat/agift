f0301ec966a7dc91e9f0df7d8b5ac2416ee6f85a
KondratWork <alexey.konratyev@gmail.com>
2009-01-22 15:31:03 +0300

    kit debug added

--- app_controller.php
+++ app_controller.php
@@ -1,7 +1,7 @@
 <?php
 uses('L10n');
 class AppController extends Controller {
-	var $components      = array('Acl', 'Auth', 'RequestHandler', 'Email', 'Cookie');
+	var $components      = array('Acl', 'Auth', 'RequestHandler', 'Email', 'Cookie','DebugKit.Toolbar');
     var $helpers         = array('Javascript', 'Html', 'Form', 'Menu', 'Tree');
     var $publicControllers = array('pages', 'test');
     var $uses = array('News', 'Order');
@@ -62,6 +62,18 @@ class AppController extends Controller {
 				$this->set('currentOrderTotal', $currentOrder['Order']['line_item_count'] );
 			}
 		}
+		
+		if (Configure::read('debug') == 0){
+		                        @ob_start ('ob_gzhandler');
+		                        header('Content-type: text/html; charset: UTF-8');
+		                        header('Cache-Control: must-revalidate');
+		                        $offset = -1;
+		                        $ExpStr = "Expires: " .gmdate('D, d M Y H:i:s',time() + $offset) .' GMT';
+		                        header($ExpStr);
+		                } 
+		
+
+
 	}
 
 
